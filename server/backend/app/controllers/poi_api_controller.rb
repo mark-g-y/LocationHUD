d6db055d822939f7f0affe757e001f8ef7e7f7ea
Mark Ye <gye.2411@gmail.com>
2014-11-23 20:38:42 -0800

    Setup basic framework for creating new POI's

--- server/backend/app/controllers/poi_api_controller.rb
+++ server/backend/app/controllers/poi_api_controller.rb
@@ -3,10 +3,15 @@ require 'json'
 
 class PoiApiController < ApplicationController
 
+	protect_from_forgery with: :null_session
+
 	@@MIN_DEGREE_DIFF = 3
 	@@MIN_DISTANCE = 200
+	@@SAME_LOCATION_TOLERANCE_DISTANCE = 0.25
+	@@STRING_DIFFERENCE_THRESHOLD = 3
 	@@POI_RESULT_LIMIT = 10
 
+	# GET request
 	def index 
 		lat = params[:latitude].to_f
 		long = params[:longitude].to_f
@@ -22,6 +27,48 @@ class PoiApiController < ApplicationController
 		render :json => JSON.pretty_generate(location_list)
 	end
 	
+	#POST request
+	def create
+		# <TODO> insert spam filter here
+		
+		poi_list = JSON.parse(request.body.read)
+		
+		for poi in poi_list
+			name = poi['name']
+			lat = poi['latitude']
+			long = poi['longitude']
+			altitude = poi['altitude']
+			similar_locations = Poi.select('*, get_distance(latitude, longitude, %f, %f) as distance' % [Poi.sanitize(lat), Poi.sanitize(long)])
+				.where('get_distance(latitude, longitude, ?, ?) < ?', lat, long, @@SAME_LOCATION_TOLERANCE_DISTANCE)
+				.order('distance asc')
+			best_match_name = get_best_name_match(name, similar_locations)
+		end
+		
+		#render :json => JSON.pretty_generate(poi_list
+		render :text => best_match_name
+		
+		# insert into raw poi table - these might be useful for later analysis.
+		# commented out for now to avoid spamming my database during testing
+		#PoiRaw.create(name: "name", latitude: 'lat', longitude: 'long', altitude: 'alt', time_uploaded: Time.now
+		
+	end
+	
+	def get_best_name_match(name, potential_locations)
+		potential_locations.each do |row|
+			if do_names_match(name, row['name'])
+				return row['name']
+			end
+		end
+		return name
+	end
+	
+	def do_names_match(name1, name2)
+		name1.downcase!
+		name2.downcase
+		# <TODO> add string similarity check to see how different they are
+		return true # DEBUG for now
+	end
+	
 	def generate_location_json_from_db_row(row)
 		obj = {}
 		obj['name'] = row['name']
@@ -30,18 +77,6 @@ class PoiApiController < ApplicationController
 		obj['altitude'] = row['altitude']
 		obj['distance'] = row['distance']
 		return obj
-	end
-
-	def test_index
-		lat = params[:latitude].to_i
-		long = params[:longitude].to_i
-		obj = {latitude:@lat, longitude:@long}
-		everything = []
-		results = ResponseType.all
-		results.each do |row|
-			everything.push({response_type:row["response_type"], description:row["description"]})
-		end
-		render :json => JSON.pretty_generate(everything)
 	end
 
 end
