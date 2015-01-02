152a3570d3e813045eb6569d34933cd03e8c0703
Mark Ye <gye.2411@gmail.com>
2015-01-02 00:16:45 -0800

    Added basic functionality to return nearby locations

--- server/parse/cloud/main.js
+++ server/parse/cloud/main.js
@@ -4,3 +4,17 @@
 Parse.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
 });
+
+Parse.Cloud.define("nearby_locations", function(request, response) {
+	var query = new Parse.Query("Location");
+	var current_location = new Parse.GeoPoint(request.params.latitude, request.params.longitude);
+	query.withinKilometers("latlong", current_location, 100);
+	query.find({
+		success : function(results) {
+			response.success(results);
+		},
+		error : function() {
+			response.error("oh no!");
+		}
+	});
+});
